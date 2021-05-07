8d1da45306fcf2147a743e4a12c617ff681f080b
ClementEraud <clement.eraud@gmail.com>
2021-05-07 13:42:38 +0200

    :construction_worker: Add CI/CD pipeline
    :fire: remove package-lock.json
    
    Add .circleci/config.yml
    
    :construction_worker: Add CI/CD pipeline
    
    💚 test ci build and deploy
    
    👷 add deploy job
    
    Updated config.yml
    
    Updated config.yml
    
    💚 fix install sshpass on circle ci image
    
    ✅ fix app controller test
    
    🚀 new hello world message
    
    🚀 again a new message
    
    :green_heart: deploy on tag on main branch

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it(`should return "Here we go !  Mario !"`, () => {
+      expect(appController.getHello()).toBe(`Here we go !  Mario !`);
     });
   });
 });
